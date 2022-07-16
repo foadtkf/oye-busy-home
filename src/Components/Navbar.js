fc7c907322837b5419654376ee510ce422157158
foadtkf <fakhrulif@gmail.com>
2022-07-16 09:41:54 +0600

    navbar created

---
+++ src/Components/Navbar.js
@@ -0,0 +1,57 @@
+import React from 'react';
+import downloadapp from "../images/DownloadApp.png"
+import logo from "../images/logo.png"
+const Navbar = () => {
+    return (
+        <div class="navbar bg-base-100">
+  <div class="navbar-start">
+    <div class="dropdown">
+      <label tabindex="0" class="btn btn-ghost lg:hidden">
+        <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
+      </label>
+      <ul tabindex="0" class="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
+        <li><a>1st Floor, H 356, Block B, Sector 22, Noida, Uttar Pradesh 201301</a></li>
+        <li tabindex="0">
+          <a class="justify-between">
+            Parent
+            <svg class="fill-current" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M8.59,16.58L13.17,12L8.59,7.41L10,6L16,12L10,18L8.59,16.58Z"/></svg>
+          </a>
+          <ul class="p-2">
+            <li><a>Submenu 1</a></li>
+            <li><a>Submenu 2</a></li>
+          </ul>
+        </li>
+        <li><a>Item 3</a></li>
+      </ul>
+    </div>
+    <ul class="menu menu-horizontal p-0">
+      <li>
+    <a class=" normal-case text-xl"><img src={logo} className="w-20"  alt=""/></a>
+      </li>
+      <li><a>1st Floor, H 356, Block B, Sector 22, Noida, Uttar Pradesh 201301</a></li>
+    </ul>
+  </div>
+
+  <div class="navbar-end hidden lg:flex">
+    <ul class="menu menu-horizontal p-0">
+      <li><a>Blog</a></li>
+      <li><a>Career</a></li>
+      <li tabindex="0">
+        <a>
+          My Account
+          <svg class="fill-current" xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24"><path d="M7.41,8.58L12,13.17L16.59,8.58L18,10L12,16L6,10L7.41,8.58Z"/></svg>
+        </a>
+        <ul class="p-2">
+          <li><a>Submenu 1</a></li>
+          <li><a>Submenu 2</a></li>
+        </ul>
+      </li>
+      <li><a><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M24 3l-.743 2h-1.929l-3.474 12h-13.239l-4.615-11h16.812l-.564 2h-13.24l2.937 7h10.428l3.432-12h4.195zm-15.5 15c-.828 0-1.5.672-1.5 1.5 0 .829.672 1.5 1.5 1.5s1.5-.671 1.5-1.5c0-.828-.672-1.5-1.5-1.5zm6.9-7-1.9 7c-.828 0-1.5.671-1.5 1.5s.672 1.5 1.5 1.5 1.5-.671 1.5-1.5c0-.828-.672-1.5-1.5-1.5z"/></svg></a></li>
+      <a><img src={downloadapp} className="w-44" style={{marginTop:'3%'}} alt=""/></a>
+    </ul>
+  </div>
+</div>
+    );
+};
+
+export default Navbar;
